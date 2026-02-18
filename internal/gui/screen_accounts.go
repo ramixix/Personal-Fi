e293fdb217ed13a3e6ca5ab4a1e752a7e83601af
ramixix <test@example.com>
2026-02-18 12:22:51 +0000

    add header of accounts screen

--- internal/gui/screen_accounts.go
+++ internal/gui/screen_accounts.go
@@ -1,6 +1,10 @@
 package gui
 
 import (
+	"financial_tracker/internal/core"
+	"financial_tracker/internal/storage"
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -15,13 +19,28 @@ func NewAccountsScreen(app *GuiApp) *AccountsScreen {
 }
 
 func (a *AccountsScreen) Render() fyne.CanvasObject {
-	title := widget.NewLabelWithStyle("Accounts", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
+	header := a.createHeader()
 
 	content := container.NewVBox(
-		title,
+		header,
 		widget.NewSeparator(),
 		widget.NewLabel("Accounts management page - Coming soon!"),
 	)
 
 	return container.NewScroll(content)
+}
+
+func (a *AccountsScreen) createHeader() fyne.CanvasObject {
+	title := widget.NewLabelWithStyle("🏦 Accounts", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
+
+	accountCount := len(storage.Accounts)
+	accountTotals := core.GetTotalAccountBalance()
+
+	statsLabel := widget.NewLabel(fmt.Sprintf("Total Accounts: %d | Total Balance: %.2f", accountCount, accountTotals))
+
+	addNewAccountBtn := widget.NewButton("Add new account", func() {})
+	addNewAccountBtn.Importance = widget.HighImportance
+
+	header := container.NewBorder(nil, nil, title, statsLabel)
+	return container.NewVBox(header, addNewAccountBtn)
 }
