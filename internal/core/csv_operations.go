ec35fad5457e453856b8c831e4404fa58666a6f1
ramixix <77832933+ramixix@users.noreply.github.com>
2026-05-11 08:43:34 +0000

    modified csv operations functions to ensure it works with new db approach (file: internal/core/csv_operations.go)

--- internal/core/csv_operations.go
+++ internal/core/csv_operations.go
@@ -3,7 +3,6 @@ package core
 import (
 	"encoding/csv"
 	"financial_tracker/internal/models"
-	"financial_tracker/internal/storage"
 	"financial_tracker/internal/utils"
 	"fmt"
 	"os"
@@ -22,18 +21,20 @@ func ExportTransactionsToCSV(filename string) error {
 	defer writer.Flush()
 
 	// Write header
-	header := []string{"ID", "Date", "Type", "Amount", "Category", "Description"}
+	header := []string{"ID", "Date", "Type", "Amount", "Currency", "Category", "Description"}
 	if err := writer.Write(header); err != nil {
 		return err
 	}
 
 	// Write data
-	for _, transaction := range storage.Transactions {
+	transactions := GetAllTransactions()
+	for _, transaction := range transactions {
 		record := []string{
-			strconv.Itoa(transaction.ID),
+			transaction.ID,
 			transaction.Date.Format("2006-01-02"),
 			transaction.Type,
 			fmt.Sprintf("%.2f", transaction.Amount),
+			transaction.CurrencyCode,
 			transaction.Category,
 			transaction.Description,
 		}
@@ -57,24 +58,25 @@ func ExportAccountsToCSV(filename string) error {
 	defer writer.Flush()
 
 	// Write header
-	header := []string{"ID", "Name", "Balance", "Created"}
+	header := []string{"ID", "Name", "Balance", "Currency", "Created"}
 	if err := writer.Write(header); err != nil {
 		return err
 	}
 
 	// Write data
-	for _, account := range storage.Accounts {
+	accounts := GetAllAccounts()
+	for _, account := range accounts {
 		record := []string{
-			strconv.Itoa(account.ID),
+			account.ID,
 			account.Name,
 			fmt.Sprintf("%.2f", account.Balance),
+			account.CurrencyCode,
 			account.Created.Format("2006-01-02"),
 		}
 		if err := writer.Write(record); err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
 
@@ -90,29 +92,21 @@ func ExportAccountTransactionsToCSV(filename string) error {
 	defer writer.Flush()
 
 	// Write header
-	header := []string{"ID", "Account ID", "Account Name", "Amount", "Date", "Note"}
+	header := []string{"ID", "Account ID", "Amount", "Date", "Note"}
 	if err := writer.Write(header); err != nil {
 		return err
 	}
 
 	// Write data
-	for _, at := range storage.AccountTransactions {
-		// Find account name
-		accountName := "Unknown"
-		for _, acc := range storage.Accounts {
-			if acc.ID == at.AccountID {
-				accountName = acc.Name
-				break
-			}
-		}
+	accountTransactions := GetAllAccountTransactions()
+	for _, accTx := range accountTransactions {
 
 		record := []string{
-			strconv.Itoa(at.ID),
-			strconv.Itoa(at.AccountID),
-			accountName,
-			fmt.Sprintf("%.2f", at.Amount),
-			at.Date.Format("2006-01-02"),
-			at.Note,
+			accTx.ID,
+			accTx.AccountID,
+			fmt.Sprintf("%.2f", accTx.Amount),
+			accTx.Date.Format("2006-01-02"),
+			accTx.Note,
 		}
 		if err := writer.Write(record); err != nil {
 			return err
@@ -133,10 +127,12 @@ func ImportTransactionsFromCSV(filename string) error {
 	reader := csv.NewReader(file)
 
 	// Read header
-	_, err = reader.Read()
+	header, err := reader.Read()
 	if err != nil {
 		return err
 	}
+
+	headerLenght := len(header)
 
 	// Read data
 	records, err := reader.ReadAll()
@@ -145,7 +141,7 @@ func ImportTransactionsFromCSV(filename string) error {
 	}
 
 	for _, record := range records {
-		if len(record) < 6 {
+		if len(record) < headerLenght {
 			continue // Skip invalid records
 		}
 
@@ -164,16 +160,20 @@ func ImportTransactionsFromCSV(filename string) error {
 
 		// Create transaction
 		transaction := models.Transaction{
-			ID:          storage.NextTransactionID,
-			Date:        date,
-			Type:        record[2],
-			Amount:      amount,
-			Category:    record[4],
-			Description: record[5],
+			ID:           record[0],
+			Date:         date,
+			Type:         record[2],
+			Amount:       amount,
+			CurrencyCode: record[4],
+			Category:     record[5],
+			Description:  record[6],
 		}
 
-		storage.Transactions = append(storage.Transactions, transaction)
-		storage.NextTransactionID++
+		err = AddTransaction(transaction)
+		if err != nil {
+			fmt.Printf("Warning: Skipping, transaction is read but could not added to db: %v\n", record)
+			continue
+		}
 	}
 
 	return nil
@@ -190,10 +190,11 @@ func ImportAccountsFromCSV(filename string) error {
 	reader := csv.NewReader(file)
 
 	// Read header
-	_, err = reader.Read()
+	header, err := reader.Read()
 	if err != nil {
 		return err
 	}
+	headerLenght := len(header)
 
 	// Read data
 	records, err := reader.ReadAll()
@@ -202,7 +203,7 @@ func ImportAccountsFromCSV(filename string) error {
 	}
 
 	for _, record := range records {
-		if len(record) < 4 {
+		if len(record) < headerLenght {
 			continue
 		}
 
@@ -212,21 +213,25 @@ func ImportAccountsFromCSV(filename string) error {
 			continue
 		}
 
-		created, err := utils.ParseDate(record[3])
+		created, err := utils.ParseDate(record[4])
 		if err != nil {
 			fmt.Printf("Warning: Skipping invalid date in row: %v\n", record)
 			continue
 		}
 
 		account := models.Account{
-			ID:      storage.NextAccountID,
-			Name:    record[1],
-			Balance: balance,
-			Created: created,
+			ID:           record[0],
+			Name:         record[1],
+			Balance:      balance,
+			CurrencyCode: record[3],
+			Created:      created,
 		}
 
-		storage.Accounts = append(storage.Accounts, account)
-		storage.NextAccountID++
+		err = AddAccount(account)
+		if err != nil {
+			fmt.Printf("Warning: Skipping, account is read but could not added to db: %v\n", record)
+			continue
+		}
 	}
 
 	return nil
